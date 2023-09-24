81e790874ad7ee94b9b3cc7f5aa2af0a4d0cb701
web131413 <106674078+web131413@users.noreply.github.com>
2023-09-24 16:07:31 +0800

    update

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -25,6 +25,9 @@ const Layout = ({ children }) => {
 
       <footer>
         <Container>
+          <h3>Medical Disclaimer</h3>
+          All content and media on the OnlyTCM is created and published online for informational purposes only. It is not intended to be a substitute for professional medical advice and should not be relied on as health or personal advice.
+          Always seek the guidance of your doctor or other qualified health professional with any questions you may have regarding your health or a medical condition. Never disregard the advice of a medical professional, or delay in seeking it because of something you have read on this Website.
           <h3>Do your own Research</h3>
           Under no circumstances OnlyTCM be held responsible or liable in any way for any claims, damages, losses, expenses, costs or liabilities whatsoever (including, without limitation, any direct or indirect damages for loss of profits, business interruption or loss of information) resulting or arising directly or indirectly from your use of or inability to use this website or any websites linked to it, or from your reliance on the information and material on this website, even if the OnlyTCM has been advised of the possibility of such damages in advance.
         </Container>
--- web/src/components/navbar.js
+++ web/src/components/navbar.js
@@ -1,6 +1,8 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { useLocation } from '@reach/router';
+const { useEffect, useState } = React;
 
 const isActive = ({ isCurrent }) => {
   return isCurrent ? { className: "nav-link active" } : { className: "nav-link" }
@@ -10,7 +12,21 @@ const ExactNavLink = props => (
   <Link getProps={isActive} {...props} />
 )
 
+const spSubmenu = [
+  {
+    to: '/',
+    label: 'Flu, Fever 感冒 发烧 '
+  }
+]
+
 const Navbar = ({ siteTitle }) => {
+  const location = useLocation();
+  const [isSPActive, setIsSPActive] = useState(false)
+  useEffect(() => {
+    const _isActive = !!spSubmenu.find(e => location.pathname.includes(e?.to))
+    setIsSPActive(_isActive)
+  }, [location.pathname])
+
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-primary">
       <div className="container-fluid">
@@ -22,11 +38,20 @@ const Navbar = ({ siteTitle }) => {
 
         <div className="collapse navbar-collapse" id="main-navbar">
           <ul className="navbar-nav me-auto mb-2 mb-md-0">
+
+            <li class="nav-item dropdown" id="myDropdown">
+              <a class={`nav-link dropdown-toggle ${isSPActive ? 'active' : ''}`} href="#" data-bs-toggle="dropdown">  Specific Treatment 专治  </a>
+              <ul class="dropdown-menu">
+                {
+                  spSubmenu?.map(sm => <li> <Link className="dropdown-item" to={sm?.to}> { sm.label } </Link></li>)
+                }
+              </ul>
+            </li>
             <li className="nav-item">
               <ExactNavLink
                 to="/"
               >
-                Search
+                Search 搜索
               </ExactNavLink>
             </li>
             <li className="nav-item">
